80e6d78bb78a9ce4cc67c3004a34bb49ad487883
Jonáš Rosecký <rosecky.jonas@gmail.com>
2025-07-08 14:11:47 +0200

    Added required dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 import fs from "fs";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/nfc-bank/",
     plugins: [
         react(),
         VitePWA({
